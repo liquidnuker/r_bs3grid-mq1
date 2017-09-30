25df8ba1dd83dff651060d46102aaa1407de58f6
administrator <liquidnuker@users.noreply.github.com>
2017-09-29 20:37:28 -0700

    testing purify css...

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
+const glob = require('glob-all');
 const webpack = require('webpack');
 const Promise = require('es6-promise').Promise;
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 const extractCSS = new ExtractTextPlugin('../[name].bundle.css');
 
 module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
     index: './index.js'
-    // vendor: ['jquery', 'lodash']
+      // vendor: ['jquery', 'lodash']
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -34,8 +34,7 @@ module.exports = {
             limit: 10000
           } // Convert images < 10k to base64 strings
         }]
-      },
-      {
+      }, {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -65,7 +64,15 @@ module.exports = {
     // new webpack.optimize.CommonsChunkPlugin({
     //   name: 'vendor',
     // }),
-    extractCSS
+    extractCSS,
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync([
+        path.join(__dirname, '.html'),
+        // path.join(__dirname, 'partials/.php')
+      ]),
+    })
+
   ],
   resolve: {
     modules: [
