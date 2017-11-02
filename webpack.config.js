6a9d404d5c5107bd29893c2ba1ae130995fad025
administrator <liquidnuker@users.noreply.github.com>
2017-11-02 09:58:32 -0700

    added vendor/bulma

--- webpack.config.js
+++ webpack.config.js
@@ -8,62 +8,64 @@ const PurifyCSSPlugin = require('purifycss-webpack');
 const extractCSS = new ExtractTextPlugin('../[name].bundle.css');
 
 module.exports = {
-  context: path.resolve(__dirname, './src'),
-  // Map, Set, requestAnimationFrame <IE11 polyfill
-  // entry: ['babel-polyfill', './index.jsx'],
-  entry: ['./index.js'],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: "dist/",
-    filename: '[name].bundle.js',
-    chunkFilename: '[id].chunk.js'
-  },
-  module: {
-    rules: [
-      // extractCSS
-      {
-        test: /\.scss$/,
-        loader: extractCSS.extract([
-          {
-            loader: 'css-loader',
-            // options: { minimize: true }
-          }, 
-          'sass-loader'])
+	context: path.resolve(__dirname, './src'),
+	// Map, Set, requestAnimationFrame <IE11 polyfill
+	// entry: ['babel-polyfill', './index.jsx'],
+	entry: ['./index.js'],
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: "dist/",
+		filename: '[name].bundle.js',
+		chunkFilename: '[id].chunk.js'
+	},
+	module: {
+		rules: [
+			// extractCSS
+			{
+				test: /\.scss$/,
+				loader: extractCSS.extract([{
+						loader: 'css-loader',
+						// options: { minimize: true }
+					},
+					'sass-loader'
+				])
+			},
+			// url loader
+			{
+				test: /\.(png|svg|jpg|otf|ttf)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 10000
+					} // Convert images < 10k to base64 strings
+				}]
+			},
+			// babel-loader
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+		]
+	},
+	externals: {},
+	plugins: [
+		extractCSS,
+		new PurifyCSSPlugin({
+			// Give paths to parse for rules. These should be absolute!
+			paths: glob.sync([
+				path.join(__dirname, '*.html'),
+				// path.join(__dirname, 'src/components/*.jsx')
+			]),
+			purifyOptions: {
+        whitelist: [ '*:not*' ]
       },
-      // url loader
-      {
-        test: /\.(png|svg|jpg|otf|ttf)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000
-          } // Convert images < 10k to base64 strings
-        }]
-      },
-      // babel-loader
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-    ]
-  },
-  externals : {
-  },
-  plugins: [
-    extractCSS,
-    new PurifyCSSPlugin({
-      // Give paths to parse for rules. These should be absolute!
-      paths: glob.sync([
-        path.join(__dirname, '*.html'),
-        // path.join(__dirname, 'src/components/*.jsx')
-      ]),
-      minimize: true
-    }),
-  ],
-  resolve: {
-    modules: [
-      '../node_modules',
-    ]
-  }
+			minimize: true
+		}),
+	],
+	resolve: {
+		modules: [
+			'../node_modules',
+		]
+	}
 };
